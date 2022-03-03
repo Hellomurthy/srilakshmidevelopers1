108727c37f733785aeef671a87ddbbc3ed0246a1
tee swizzle <letemailmurthy@gmail.com>
2022-03-03 00:53:44 -0500

    Home page updated

--- src/App.js
+++ src/App.js
@@ -4,32 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <Router>
-       <Switch>
-         <Route path='/srilakshmidevelopers' exact component={Homepage}/>
-         <Route path='/problem-srilakshmidevelopers/public/index.html' exact component={SecondPage}/>
-       </Switch>
-     </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
