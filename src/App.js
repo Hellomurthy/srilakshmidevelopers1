42fe056a4955b117503b82f8c5fcaf9ed7b0d8f1
tee swizzle <letemailmurthy@gmail.com>
2022-03-03 20:15:19 -0500

    First Version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/navbar'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        Welcome to Sri Lakshmi Developers
-      <br></ br>
-      building our web site, just like how passionate we are to build your dream home...
-      </header>
+    <div>
+      <Navbar />
     </div>
   );
 }
