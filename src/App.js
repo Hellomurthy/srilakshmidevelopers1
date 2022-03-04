3fd60843143629edb34457a1c9df755e7d30f4b7
tee swizzle <letemailmurthy@gmail.com>
2022-03-03 21:18:43 -0500

    Basic Update pages Home/About/Projects/Services/Contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 import Navbar from './Components/navbar'; 
+import BannerImage from './assets/Home.JPG'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Contact" element={<Contact />} />         
+        </Routes>
+      </Router>     
     </div>
   );
 }
